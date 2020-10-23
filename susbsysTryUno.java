c284f1b814409cba81a2168e855eff7582c83835
Hutton Grabiel <huttonthomas@icloud.com>
2020-10-23 00:02:25 -0700

    added climbersys

--- susbsysTryUno.java
+++ susbsysTryUno.java
@@ -1,5 +1,8 @@
 public class susbsysTryUno {
 
+    private ClimberSys() {
+        mLog = new LogWrapper(FileType.SUBSYSTEM,"Intake", LoggerConstants.INTAKE_PERMISSION;
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
     }
